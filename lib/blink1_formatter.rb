77b3062a96e37b07dcb6df4b7c3e944ab2dad50c
Atsushi NAGASE <atsn.ngs@gmail.com>
2012-12-19 13:26:55 +0900

    bugfix

--- lib/blink1_formatter.rb
+++ lib/blink1_formatter.rb
@@ -4,48 +4,49 @@ require "rspec/core/formatters/base_formatter"
 class Blink1Formatter < RSpec::Core::Formatters::BaseFormatter
 
   COLOR_SUCCESS = [0,   255, 35]
-  COLOR_PENDING = [255, 240,  6]
-  COLOR_FAIL    = [255,   7, 29]
-  
+  COLOR_PENDING = [255, 200,  0]
+  COLOR_FAIL    = [255,  10, 30]
+
   def example_passed(example)
-    unless @color
-      @color = COLOR_SUCCESS
-      update_color
-    end
+    self.color= COLOR_SUCCESS if self.color.eql?(COLOR_SUCCESS)
     super(example)
   end
 
   def example_pending(example)
-    if @color == COLOR_SUCCESS
-      @color = COLOR_PENDING
-      update_color
-    end
+    self.color= COLOR_PENDING unless self.color.eql?(COLOR_FAIL)
     super(example)
   end
 
   def example_failed(example)
-    if @color != COLOR_FAIL
-      @color = COLOR_FAIL
-      update_color
-    end
+    self.color= COLOR_FAIL
     super(example)
   end
 
   def dump_summary(duration, example_count, failure_count, pending_count)
-    @blink1.blink(@color[0], @color[1], @color[2], 2)
-    @blink1.fade_to_rgb(500, @color[0], @color[1], @color[2])
     super(duration, example_count, failure_count, pending_count)
+    blink1.blink(color[0], color[1], color[2], 2)
+    blink1.fade_to_rgb(500, color[0], color[1], color[2])
   end
 
-  private
-  
-    def update_color
-      unless @blink1
-        @blink1 = Blink1.new
-        @blink1.open
-      end
-      @blink1.fade_to_rgb(300, @color[0], @color[1], @color[2])
+  def blink1
+    unless @blink1
+      @blink1 = Blink1.new
+      @blink1.open
     end
+    @blink1
+  end
+
+  def color= c
+    unless c.eql?(@color)
+      @color = c
+      blink1.fade_to_rgb(300, c[0], c[1], c[2])
+    end
+    c
+  end
+  
+  def color
+    @color || COLOR_SUCCESS
+  end
 
 
 end
--- lib/blink1_formatter/version.rb
+++ lib/blink1_formatter/version.rb
@@ -1,3 +1,3 @@
 class Blink1Formatter
-  VERSION = "0.0.3"
+  VERSION = "0.0.4"
 end
